cdd4b643121b6ea7cb0e62c898e25099f101f4cc
dalong <dalong0514@126.com>
2020-07-29 11:00:01 +0800

    18.08-code

--- laravel/tests/Unit/UserStoreTest.php
+++ laravel/tests/Unit/UserStoreTest.php
@@ -1,5 +1,5 @@
 <?php
-
+// 18.08-code
 namespace Tests\Unit;
 
 use App\Code\UserStore;
@@ -25,5 +25,19 @@ class UserStoreTest extends TestCase
         $this->assertEquals('a@b.com', $user['mail']);
         $this->assertEquals('bob williams', $user['name']);
         $this->assertEquals('12345', $user['pass']);
+    }
+
+    public function testAddUserDuplicate() {
+        try {
+            $ret = $this->store->addUser('bob williams', 'a@b.com', '12345');
+            $ret = $this->store->addUser('bob stevens', 'a@b.com', '12345');
+            self::fail('Exception should have been thrown');
+        } catch (\Exception $e) {
+            $const = $this->logicalAnd(
+                $this->logicalNot($this->contains('bob stevens')),
+                $this->isType('array')
+            );
+            self::assertThat($this->store->getUser('a@b.com'), $const);
+        }
     }
 }
